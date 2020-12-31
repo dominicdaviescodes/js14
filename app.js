2a331fe4c2ab9e8c88a06fda37e1d14053797037
dominicdaviescodes <codewithdom@gmail.com>
2020-12-31 18:30:19 +0000

    add coding ex 20 and notes on random numbers

--- app.js
+++ app.js
@@ -1,10 +1,28 @@
+// Ex20
+
+// NO TOUCHING! (please)
+const die1 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+const die2 = Math.floor(Math.random() * 6) + 1; //random number from 1-6
+
+// YOUR CODE BELOW THIS LINE:
+let roll = `You rolled a ${die1} and a ${die2}. They sum to ${die1+die2}`;
+console.log(roll)
+
+
+
+
+
+
+
+
+
 // DON'T TOUCH THIS LINE! (please)
-const word = 'skateboard'; //Don't change this line!
+// const word = 'skateboard'; //Don't change this line!
 
 // YOUR CODE BELOW THIS LINE:
 
-const word2 = word.slice(-5);
+// const word2 = word.slice(-5);
 
 
-const facialHair = word2.replace('o', 'e');
-console.log(facialHair)
+// const facialHair = word2.replace('o', 'e');
+// console.log(facialHair)
