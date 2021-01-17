a3f27cf89922640b7f3f9860c8a832ae6d2c20d0
dominicdaviescodes <codewithdom@gmail.com>
2021-01-17 20:10:18 +0000

    course notes to L200

--- loops/ForOf/app.js
+++ loops/ForOf/app.js
@@ -1,12 +1,16 @@
-const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'gunners'];
+// const subreddits = ['cringe', 'books', 'chickens', 'funny', 'pics', 'soccer', 'gunners'];
 
-// for (let i = 0; i < subreddits.length; i++) {
-//     console.log(`Visit reddit.com/r/${subreddits[i]}`)
+// for (let subreddit of subreddits) {
+//     console.log(`Visit reddit.com/r/${subreddit}`)
 // }
 
-for (let subreddit of subreddits) {
-    console.log(`Visit reddit.com/r/${subreddit}`)
-}
+
+// for (let i = 0; i < subreddits.length; i++) {
+//   console.log(`Visit reddit.com/r/${subreddits[i]}`);
+// }
+
+
+
 
 
 
@@ -16,20 +20,37 @@ for (let subreddit of subreddits) {
 //     ['Yuma', 'Sakura', 'Jack', 'Erika']
 // ]
 
-// for (let i = 0; i < seatingChart.length; i++) {
-//     const row = seatingChart[i];
-//     for (let j = 0; j < row.length; j++) {
-//         console.log(row[j])
-//     }
-// }
-
 // for (let row of seatingChart) {
 //     for (let student of row) {
 //         console.log(student);
 //     }
 // }
 
+
+// for (let i = 0; i < seatingChart.length; i++) {
+//   const row = seatingChart[i];
+//   for (let j = 0; j < row.length; j++) {
+//     console.log(row[j]);
+//   }
+// }
+
+// Itterate over when not an array. this is a str
 // for (let char of "hello world") {
 //     console.log(char)
 // }
 
+// Ex. 34
+// print these numbers using for of
+// const numbers = [1,2,3,4,5,6,7,8,9]; 
+
+// for (let num of numbers){
+//     console.log(num)
+// }
+
+// Ex. 34a
+// loop over and print sq route of each item
+const numbers = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+for(let num of numbers){
+    console.log(num * num)
+}
